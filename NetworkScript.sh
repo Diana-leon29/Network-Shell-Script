24a691fa0406eebf4f2730995a28792c10df2b07
mati <mati@gmail.com>
2024-02-16 16:36:41 -0500

    New feature added to display routing table

--- NetworkScript.sh
+++ NetworkScript.sh
@@ -11,3 +11,15 @@ echo "1. chechinh Internet Connectivity"
 ping -c 4 www.google.com 
 
 echo "---------------------"
+
+# Display Network Interfaces and Ip Address
+
+echo "2. Displaying Network Interfaces and Ip Address"
+ipconfig
+echo "------------------"
+
+#Display Roting table 
+
+echo "3. Display Routing Table"
+neststat -rn
+echo "-----------------------"
