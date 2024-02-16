c2a043fc037aaa3c6483e47ec8f27140756467cd
mati <mati@gmail.com>
2024-02-16 14:15:25 -0500

    estamos en linea

---
+++ NetworkScript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Network testing script for git bash on Windows
+
+echo "Network Testing Script"
+
+#Check Internet Connectivity 
+
+echo "1. chechinh Internet Connectivity"
+
+ping -c 4 www.google.com 
+
+echo "---------------------"
